18e0656b99b54c2c77827e3fede48d271e787e19
Kerim TONKAZ <58753458+kerimtonkaz@users.noreply.github.com>
2020-11-20 01:36:10 +0300

    Update server.lua

--- rh-kampkur/server.lua
+++ rh-kampkur/server.lua
@@ -14,16 +14,10 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
 ESX.RegisterUsableItem('kampkiti', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
     xPlayer.removeInventoryItem('kampkiti', 1)
-    TriggerClientEvent('rh:kampkur', source)
-    Citizen.Wait(13000)
+    TriggerClientEvent('rh:kampkur', source)    
 end)
 
-
-RegisterServerEvent('rh:kampkontrol')
-AddEventHandler('rh:kampkontrol', function()
-    TriggerClientEvent("rh-kampkur:createobject",source,'prop_beach_fire','prop_skid_tent_01','prop_skid_chair_02')
-end)
+RegisterServerEvent('rh:kampkontrol')AddEventHandler('rh:kampkontrol',function()TriggerClientEvent("rh-kampkur:createobject",source,'prop_beach_fire','prop_skid_tent_01','prop_skid_chair_02')end)
