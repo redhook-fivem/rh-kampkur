06c3bac0708282f7d694dc136365ffe8df6049b5
Kerim TONKAZ <58753458+kerimtonkaz@users.noreply.github.com>
2020-11-20 00:25:27 +0300

    Update server.lua

--- rh-kampkur/server.lua
+++ rh-kampkur/server.lua
@@ -9,5 +9,14 @@
 --$$ |  $$ |$$       |$$    $$ |$$ |  $$ |$$    $$/ $$    $$/ $$ | $$  |
 --$$/   $$/  $$$$$$$/  $$$$$$$/ $$/   $$/  $$$$$$/   $$$$$$/  $$/   $$/ 
 --@Bu script mike tarafından kodlanıp, redhook dev takımı tarafından geliştirilmiştir.by base64
-                                                                                                                                    
-ESX=nil;TriggerEvent('esx:getSharedObject',function(a)ESX=a end)ESX.RegisterUsableItem('kampkiti',function(b)local c=ESX.GetPlayerFromId(b)TriggerClientEvent('rh:kampkur',b)Citizen.Wait(13000)TriggerClientEvent("rh-kampkur:createobject",b,'prop_beach_fire','prop_skid_tent_01','prop_skid_chair_02')c.removeInventoryItem('kampkiti',1)end)
+
+ESX = nil 
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+
+ESX.RegisterUsableItem('kampkiti', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    TriggerClientEvent('rh:kampkur', source)
+    xPlayer.removeInventoryItem('kampkiti', 1)
+end)
